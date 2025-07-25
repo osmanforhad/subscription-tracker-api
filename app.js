446c648292483b48f2d619bafe43264dea3ce769
osmanforhad <osmanforhad93@gmail.com>
2025-07-25 09:24:17 +0600

    express server setup

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Server runing on localhost: 8000");
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to the Subscription Tracker API!");
+});
+
+app.listen(3000, () => {
+    console.log("Subscription Tracker API is runing on http://localhost:3000");
+});
+
+export default app;
